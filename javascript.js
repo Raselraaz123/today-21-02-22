96a0bee28fab7289c0f7ecb5c4f12779ae4594d7
Raselraaz123 <82328669+Raselraaz123@users.noreply.github.com>
2022-02-21 10:35:35 +0600

    total

--- javascript.js
+++ javascript.js
@@ -1,1 +1,118 @@
-console.log('Rasel raaz')
+// console.log('Rasel raaz')
+// var a = 10;
+// console.log(a)
+
+// console.log('wrong');
+// var b = 'Rana';
+// var c = b + (' is from bangladash');
+// console.log(c)
+// var _rasel = 499;
+// console.log(_rasel)
+// var $to = 'romsn';
+// console.log($to)
+// var num_of = 500;
+// console.log(num_of);
+// let myName = 'rasel';
+// console.log(myName);
+// let myNames = 'rana';
+// console.log(myNames);
+
+let myMame = 'Rasel Rana';
+let fullName = "md " + myMame;
+console.log(fullName);
+var aNamber = 40;
+console.log(typeof (aNamber));
+
+var mco = 'jafor'
+var bn = "gofor";
+var hoo = "300"
+console.log(mco + ' ' + bn + " " + hoo)
+console.log(typeof (mco + ' ' + bn + " " + hoo))
+var ab = true;
+var ba = false;
+console.log(ab)
+var nana;
+console.log(nana)
+if (ab == false) {
+    console.log('rasel in')
+}
+else if (ba == true) {
+    console.log('roman in')
+}
+else if (ab == true) {
+    console.log("rasel out of the fild")
+}
+else {
+    console.log('code out')
+}
+var uoo = null;
+console.log(uoo)
+var text = 'How' + ' are you Broo'
+var welcomeMsg = 'Hello ' + text;
+console.log(welcomeMsg);
+console.log(typeof (welcomeMsg));
+let bo = 50;
+let co = 90 - bo;
+console.log(co);
+console.log(typeof (co));
+const ro = 50;
+const qp = 'Rasel ' + ro;
+console.log(qp);
+console.log(typeof (qp));
+const top = 30 + 50 + 10 + 5 + 5;
+const ew = top + " Raaz " + 500 + " " + 300;
+console.log(ew);
+console.log(typeof (ew));
+var ym = 'jamal';
+ym = 'go'
+console.log(ym)
+var uo = 'wooo goo';
+console.log(uo);
+console.log(typeof (uo));
+uo = 400;
+console.log(typeof (uo));
+let ioo;
+console.log(ioo)
+let myNames = 'Rasel Rana Raaz';//this is my comment singal
+console.log(myNames)
+const multi = 50 * 5; 
+/*this is my new mulity line comment?*/
+var conplex = 10 + 10 + 'tomato' + 10;
+console.log(conplex);
+var something = 390;
+if (something>5000) {
+    console.log('rooo')
+}
+else {
+    console.log("pooo")
+
+}
+let oh = 30 + 40;
+console.log(oh)
+var wo = 100 - 30;
+console.log(wo)
+const yo = oh * wo;
+console.log(yo)
+const eo = yo / 2;
+console.log(eo)
+const rop = eo % 4;
+console.log(rop)
+oh++;
+
+console.log(oh)
+wo--;
+console.log(wo);
+var pg = 60;
+// pg++
+console.log(pg)
+++pg
+console.log(pg)
+var a = 40;
+var b = 40;
+const same = a == b;
+console.log(same);
+var gow = 50;
+var qep = 20;
+var ww = gow == qep;
+console.log(ww)
+
